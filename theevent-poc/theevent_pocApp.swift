984521cd3d39d37a02d1f47f324b818860951a57
Felipe Gil <fgilcnd@gmail.com>
2023-10-19 13:59:54 -0300

    Foirst commit / blank project

---
+++ theevent-poc/theevent_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  theevent_pocApp.swift
+//  theevent-poc
+//
+//  Created by Felipe Gil on 2023-10-19.
+//
+
+import SwiftUI
+
+@main
+struct theevent_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
