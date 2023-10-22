da9d6a5a923b10f5268afd60a1f9b3c50b7bf579
Felipe Gil <fgilcnd@gmail.com>
2023-10-22 15:24:08 -0300

    Added tests

--- EventAPI/Tests/Mocks.swift
+++ EventAPI/Tests/Mocks.swift
@@ -5,5 +5,35 @@
 //  Created by Felipe Gil on 2023-10-19.
 //
 
-//TODO: Implement tests
 import Foundation
+import EventAPI
+
+class MockNetworkService: NetworkService {
+    var data: Data?
+    var statusCode: Int?
+    var error: Error?
+    var request: URLRequest?
+    
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+    ) -> NetworkServiceDataTask {
+        self.request = request
+        return MockNetworkServiceDataTask {
+            let response = HTTPURLResponse(url: request.url!, statusCode: self.statusCode ?? 200, httpVersion: nil, headerFields: nil)
+            
+            completionHandler(self.data, response, self.error)
+        }
+    }
+}
+
+struct MockNetworkServiceDataTask: NetworkServiceDataTask {
+    let resumeCompletionHandler: () -> Void
+    
+    init (resumeCompletionHandler: @escaping () -> Void) {
+        self.resumeCompletionHandler = resumeCompletionHandler
+    }
+    func resume() {
+        return resumeCompletionHandler()
+    }
+}
